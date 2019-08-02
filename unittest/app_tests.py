d4ed89305384648383704ef66a0fd33f11e0a5f0
zjdavid2 <a@ddavid.net>
2019-08-02 21:10:47 +0800

    Fix Bugs

--- unittest/app_tests.py
+++ unittest/app_tests.py
@@ -12,11 +12,15 @@ class AppTestCase(unittest.TestCase):
         pass
 
     def test_search(self):
-        rv = self.app.get('/api/searchByWords')
+        rv = self.app.post('/api/searchByWords')
         assert b'Not Implemented' in rv.data
 
     def test_get_detail(self):
         rv = self.app.get('/api/getDetail')
+        assert b'Not Implemented' in rv.data
+
+    def test_get_full_tag_list(self):
+        rv = self.app.get('/api/getFullTagList')
         assert b'Not Implemented' in rv.data
 
 

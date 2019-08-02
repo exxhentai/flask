282b0c07145d717e67becba70641260f23f85dbc
zjdavid2 <a@ddavid.net>
2019-08-02 20:47:00 +0800

    Provides Default Values

--- app/views/api.py
+++ app/views/api.py
@@ -7,18 +7,18 @@ api = Blueprint('api', __name__)
 @api.route('/api/searchByWords', methods=['POST'])
 def search_by_words():
     """ 主要搜索API """
-    json = request.get_json()
-    words = json['word']
+    input_json = request.get_json(force=True, silent=True) or {}
+    words = input_json.get('word', default='')
     #: (optional) 搜索的关键字，如果为空则返回所有结果
-    tag_list = json['tags']
-    #: (optional) 搜索的Tags，格式为Array<Str>
-    limit = json['lim']
+    tag_list = input_json.get('tags', default=[])
+    #: (optional) 搜索的Tags，格式为List<Str>
+    limit = input_json.get('limit', default=25)
     #: (optional) 搜索的结果数，默认25
-    page = json['pg']
-    #: (optional) 第几页的内容，默认1
-    filtered_category = json['filtered']
-    #: (optional) 不包含的分类
-    minimum_rating = json['minimum']
+    page = input_json.get('page', default=0)
+    #: (optional) 第几页的内容，默认0
+    filtered_category = input_json.get('filtered', default=[])
+    #: (optional) 不包含的分类，格式为List<Str>
+    minimum_rating = input_json.get('minimum', default=0.0)
     #: (optional) 最低评分
     return search.search(words, limit, page, filtered_category, minimum_rating, tag_list)
 

75b613be2fae9c5e203911e18cec760052313fd7
zjdavid2 <a@ddavid.net>
2019-08-07 09:58:01 +0800

    允许跨域请求

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from app import search, get_detail_using_gid
 import config
 from app.views.api import api
@@ -14,6 +14,16 @@ def create_app(config_file=config.Config):
     @app.route('/')
     def hello_world():
         return 'Hello World!'
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        if request.method == 'OPTIONS':
+            response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+            headers = request.headers.get('Access-Control-Request-Headers')
+            if headers:
+                response.headers['Access-Control-Allow-Headers'] = headers
+        return response
 
     return app
 

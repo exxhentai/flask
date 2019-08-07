6f86235f847b4a1f948be4fff473fbdb4efa6fbd
zjdavid2 <a@ddavid.net>
2019-08-07 10:14:07 +0800

    Add Cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app import search, get_detail_using_gid
 import config
 from app.views.api import api
 from app.views.upload import upload
+from flask_cors import CORS
 
 
 def create_app(config_file=config.Config):
@@ -10,20 +11,11 @@ def create_app(config_file=config.Config):
     app.config.from_object(config_file)
     app.register_blueprint(api)
     app.register_blueprint(upload)
+    CORS(app)
 
     @app.route('/')
     def hello_world():
         return 'Hello World!'
-
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        if request.method == 'OPTIONS':
-            response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
-            headers = request.headers.get('Access-Control-Request-Headers')
-            if headers:
-                response.headers['Access-Control-Allow-Headers'] = headers
-        return response
 
     return app
 

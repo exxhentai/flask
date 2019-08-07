fe8a7590381f868a9eb5db05a3c45d79f0860053
zjdavid2 <a@ddavid.net>
2019-08-07 09:56:08 +0800

    Add more tests

--- unittest/app_tests.py
+++ unittest/app_tests.py
@@ -25,12 +25,14 @@ class AppTestCase(unittest.TestCase):
         assert isinstance(json_response, dict)
         assert isinstance(json_response['title'], str)
 
+    def test_get_detail_non_exist_gid(self):
         # 测试一个不存在的Gid
         invalid_gid = '99999999999'
         rv = self.app.get('/api/getDetail?gid=' + invalid_gid)
         json_response = rv.json
         assert json_response == RequestError().record_not_found()  # 必须返回"条目不存在"
 
+    def test_get_detail_no_gid(self):
         # 测试不提供Gid
         invalid_gid = ''
         rv = self.app.get('/api/getDetail?gid=' + invalid_gid)
@@ -48,12 +50,27 @@ class AppTestCase(unittest.TestCase):
         verify_record_json = self.app.get('/api/getDetail?gid=' + valid_gid).json
         assert verify_record_json['ipfs_url'] == ipfs_hash
 
+    def test_update_ipfs_folder_hash_non_exist_gid(self):
         # 测试一个不存在的Gid
         invalid_gid = '99999999999'
+        ipfs_hash = hex(random.getrandbits(128))
         rv = self.app.post('/upload/setIPFSFolderHashByGid', json={'gid': invalid_gid, 'ipfs_hash': ipfs_hash})
         json_response = rv.json
-        print(json_response)
         assert json_response['error'] == RequestError().record_not_found()  # 必须返回"条目不存在"
+
+    def test_update_ipfs_folder_hash_no_gid(self):
+        # 测试不提供 Gid
+        ipfs_hash = hex(random.getrandbits(128))
+        rv = self.app.post('/upload/setIPFSFolderHashByGid', json={'ipfs_hash': ipfs_hash})
+        json_response = rv.json
+        assert json_response['error'] == RequestError('gid').required_parameter_not_found()  # 必须返回"参数未找到"
+
+    def test_update_ipfs_folder_hash_no_hash(self):
+        # 测试不提供 IPFS Hash
+        invalid_gid = '99999999999'
+        rv = self.app.post('/upload/setIPFSFolderHashByGid', json={'gid': invalid_gid})
+        json_response = rv.json
+        assert json_response['error'] == RequestError('ipfs_hash').required_parameter_not_found()  # 必须返回"参数未找到"
 
     def test_get_full_tag_list(self):
         rv = self.app.get('/api/getFullTagList')
